9b6a645e534034592499dd12110a65f16306d327
Tyler Casperson <tylercasperson@gmail.com>
2020-01-27 14:33:25 -0700

    styled the into and final page

--- assets/javascript/finalTally.js
+++ assets/javascript/finalTally.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
 
     console.log(localStorage.getItem("numberCorrect"));
     console.log($(".results"));
-    $(".results").text(localStorage.getItem("numberCorrect"));
+    var numberCorrect = localStorage.getItem("numberCorrect")
+    $(".results").text("Congragulations! You scored: " + numberCorrect); 
+    $(".results").append("<br><button class='playAgain'><a href='./index.html'>Play Again?</a></button><button class='study'><a href='https://www.omniglot.com/language/numbers/index.htm'>Study materials?</a></button>");
+
+ 
+
         
 }); //end of document.ready
